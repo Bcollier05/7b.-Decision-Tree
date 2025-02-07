03845306e9c36c766560bb78c558d5f5d9aa786c
Britney <166865959+Bcollier05@users.noreply.github.com>
2025-02-07 11:11:15 -0800

    decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
+# AUTHOR: Britney Collier
+# FILENAME: decision_tree.py
 # SPECIFICATION: description of the program
 # FOR: CS 4210- Assignment #1
 # TIME SPENT: how long it took you to complete the assignment
